e2aa53addd71d902b98ab9787414371319005536
Dmitry Zhukov <juckow.d@yandex.ru>
2025-02-11 21:42:56 +0500

    Выполнить 6ФЗ Реализовать неограниченную историю просмотров Реализовать удаление из истории при удалении задачи Реализовать исключение повтора в истории Реализовать Юнит-Тесты

--- src/controllers/InMemoryHistoryManager.java
+++ src/controllers/InMemoryHistoryManager.java
@@ -4,12 +4,9 @@ import model.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-
-   private final List<Task> history = new LinkedList<>();
 
    private final HashMap<Integer,Node> nodes = new HashMap<>();
 
@@ -27,7 +24,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     @Override
-    public List<Task> getHistory(){
+    public List<Task> getHistory() {
         return getTasks();
     }
 
